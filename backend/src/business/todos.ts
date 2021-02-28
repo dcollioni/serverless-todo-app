6843e14606ff9c0ac7b61bdd514c4728f72f6ed3
Douglas Collioni <douglascollioni@bluebridgetech.com>
2021-02-28 20:13:44 +0000

    Implement generate upload url function

--- backend/src/business/todos.ts
+++ backend/src/business/todos.ts
@@ -1,14 +1,26 @@
 import * as uuid from 'uuid'
 import TodoItem from '../models/TodoItem'
+import UploadUrl from '../models/UploadUrl'
 import TodoRepo from '../data/TodoRepo'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { parseUserId } from '../auth/utils'
 import { UpdateItemOutput, DeleteItemOutput } from 'aws-sdk/clients/dynamodb'
+import * as AWS  from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const s3 = new XAWS.S3({
+  signatureVersion: 'v4'
+})
+
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 const todoRepo = new TodoRepo()
 
-export async function getAllTodos(jwtToken: string): Promise<TodoItem[]> {
+export function getAllTodos(jwtToken: string): Promise<TodoItem[]> {
   const userId = parseUserId(jwtToken)
   return todoRepo.getAllTodos(userId)
 }
@@ -27,7 +39,8 @@ export function createTodo(
     name: createTodoRequest.name,
     dueDate: createTodoRequest.dueDate,
     createdAt: new Date().toISOString(),
-    done: false
+    done: false,
+    attachmentUrl: `https://${bucketName}.s3.amazonaws.com/${todoId}`,
   })
 }
 
@@ -53,4 +66,14 @@ export function deleteTodo(
 
   const userId = parseUserId(jwtToken)
   return todoRepo.deleteTodo(todoId, userId)
+}
+
+export function generateUploadUrl(todoId: string): UploadUrl {
+  const uploadUrl = s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: urlExpiration
+  })
+
+  return { uploadUrl }
 }
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,11 +1,22 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { createLogger } from '../../utils/logger'
+import { generateUploadUrl } from './../../business/todos'
+
+const logger = createLogger('auth')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-  console.log(todoId)
+  logger.info('Processing event: ', event)
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const todoId = event.pathParameters.todoId
+  const uploadUrl = generateUploadUrl(todoId)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(uploadUrl)
+  }
 }
